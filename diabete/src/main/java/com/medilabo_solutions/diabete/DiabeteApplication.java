be2d552ec9b98ddd38db247cbc8f5b9226324231
A-Pogam <alicepogam59@hotmail.com>
2024-07-25 15:53:30 +0200

    Created application for each micro

---
+++ diabete/src/main/java/com/medilabo_solutions/diabete/DiabeteApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo_solutions.diabete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabeteApplication.class, args);
+	}
+
+}
