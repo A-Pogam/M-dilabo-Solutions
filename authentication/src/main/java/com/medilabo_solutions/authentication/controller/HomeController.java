e93477f697afc85244a115f8bdd95cf83f4ad04a
A-Pogam <alicepogam59@hotmail.com>
2024-07-26 17:52:52 +0200

    improved pom & HomeController

--- authentication/src/main/java/com/medilabo_solutions/authentication/controller/HomeController.java
+++ authentication/src/main/java/com/medilabo_solutions/authentication/controller/HomeController.java
@@ -1,2 +1,34 @@
-package com.medilabo_solutions.authentication.controller;public class HomeController {
+package com.medilabo_solutions.authentication.controller;
+
+import com.medilabo_solutions.authentication.service.contracts.IUserService;
+import com.medilabo_solutions.authentication.model.User;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    private final Logger logger = LogManager.getLogger(HomeController.class);
+
+    @Autowired
+    private IUserService userService;
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        logger.info("Received request to access home page.");
+
+        String userEmail = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+        User user = userService.findByEmail(userEmail).orElseThrow(() -> new RuntimeException("User not found"));
+
+        model.addAttribute("user", user);
+
+        return "home";
+    }
 }
