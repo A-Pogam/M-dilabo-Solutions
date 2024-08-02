e1edf5dff5cac13c5a57bfb42be1d0d89017cf21
A-Pogam <alicepogam59@hotmail.com>
2024-08-02 17:30:12 +0200

    fixed pb mvc/Endpointlitener on gateway

--- gateway/src/main/java/com/medilabo_solutions/gateway/listener/EndpointListener.java
+++ gateway/src/main/java/com/medilabo_solutions/gateway/listener/EndpointListener.java
@@ -5,61 +5,63 @@ import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
 
 import com.medilabo_solutions.gateway.constant.UriToIgnore;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import reactor.core.publisher.Mono;
 
-public class EndpointListener implements HandlerInterceptor {
+@Component
+public class EndpointListener implements WebFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(EndpointListener.class);
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String requestURL = java.net.URLDecoder.decode(request.getRequestURL().toString(), StandardCharsets.UTF_8);
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String requestURL = java.net.URLDecoder.decode(exchange.getRequest().getURI().toString(), StandardCharsets.UTF_8);
 
-        if (!UriToIgnore.URI_TO_IGNORE.contains(request.getRequestURI())) {
-            if (!request.getParameterMap().isEmpty()) {
-                String requestParameters = "?" + request.getParameterMap().entrySet().stream().map(e -> e.getKey() + "=" + String.join(", ", e.getValue())).collect(Collectors.joining(" "));
-                logger.info("URL requested: {} {}{}", request.getMethod(), requestURL, requestParameters);
+        if (!UriToIgnore.URI_TO_IGNORE.contains(exchange.getRequest().getPath().value())) {
+            if (!exchange.getRequest().getQueryParams().isEmpty()) {
+                String requestParameters = "?" + exchange.getRequest().getQueryParams().entrySet().stream()
+                        .map(e -> e.getKey() + "=" + String.join(", ", e.getValue()))
+                        .collect(Collectors.joining(" "));
+                logger.info("URL requested: {} {}{}", exchange.getRequest().getMethod(), requestURL, requestParameters);
             } else {
-                logger.info("Endpoint requested: {} {}", request.getMethod(), requestURL);
+                logger.info("Endpoint requested: {} {}", exchange.getRequest().getMethod(), requestURL);
             }
         }
 
-        return true;
-    }
+        return chain.filter(exchange).doOnTerminate(() -> {
+            int responseStatus = exchange.getResponse().getStatusCode().value();
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) throws Exception {
-        int responseStatus = response.getStatus();
-
-        if (!UriToIgnore.URI_TO_IGNORE.contains(request.getRequestURI())) {
-            switch (responseStatus) {
-                case 200:
-                    logger.info("Response: Status {} OK", responseStatus);
-                    break;
-                case 201:
-                    String responseLocation = java.net.URLDecoder.decode(response.getHeader("Location"), StandardCharsets.UTF_8);
-                    logger.info("Response: Status {} Created - Location : {}", responseStatus, responseLocation);
-                    break;
-                case 204:
-                    logger.info("Response: Status {} No Content.", responseStatus);
-                    break;
-                case 400:
-                    logger.error("Response: Status {} Bad Request.", responseStatus);
-                    break;
-                case 404:
-                    logger.error("Response: Status {} Not Found.", responseStatus);
-                    break;
-                case 500:
-                    logger.error("Response: Status {} Internal Server Error.", responseStatus);
-                    break;
-                default:
-                    logger.error("Status unknown");
+            if (!UriToIgnore.URI_TO_IGNORE.contains(exchange.getRequest().getPath().value())) {
+                switch (responseStatus) {
+                    case 200:
+                        logger.info("Response: Status {} OK", responseStatus);
+                        break;
+                    case 201:
+                        String responseLocation = java.net.URLDecoder.decode(exchange.getResponse().getHeaders().getFirst("Location"), StandardCharsets.UTF_8);
+                        logger.info("Response: Status {} Created - Location : {}", responseStatus, responseLocation);
+                        break;
+                    case 204:
+                        logger.info("Response: Status {} No Content.", responseStatus);
+                        break;
+                    case 400:
+                        logger.error("Response: Status {} Bad Request.", responseStatus);
+                        break;
+                    case 404:
+                        logger.error("Response: Status {} Not Found.", responseStatus);
+                        break;
+                    case 500:
+                        logger.error("Response: Status {} Internal Server Error.", responseStatus);
+                        break;
+                    default:
+                        logger.error("Status unknown");
+                }
             }
-        }
+        });
     }
 }
