3dfe5f7a809af1d5f0cdb6dc16328740ea3b65f0
A-Pogam <alicepogam59@hotmail.com>
2024-08-27 18:12:49 +0200

    Missing manifest.json & fixed react display notes

--- frontend/src/pages/patientNotes.jsx
+++ frontend/src/pages/patientNotes.jsx
@@ -1,22 +1,46 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-const PatientNotes = ({ patId }) => {
+const PatientNotes = () => {
+  const { patId } = useParams(); // Récupère l'ID du patient depuis l'URL
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchNotes = async () => {
+      if (!patId) {
+        setError(new Error('Patient ID is missing'));
+        setLoading(false);
+        return;
+      }
+
       try {
         const response = await fetch(`/notes/${patId}`);
+        console.log('Response status:', response.status);
         if (!response.ok) {
           throw new Error('Network response was not ok for patient notes');
         }
         const data = await response.json();
-        setNotes(data);
+        console.log('Fetched data:', data); // Inspect the structure of the received data
+
+        // if data is a table with, at least, one object 
+        if (Array.isArray(data) && data.length > 0) {
+          // inspect the object inside the table 
+          const dataObject = data[0];
+          console.log('First object in data:', dataObject);
+
+          // check if notes is a table
+          if (Array.isArray(dataObject.notes)) {
+            setNotes(dataObject.notes);
+          } else {
+            throw new Error('Invalid data format: notes should be an array');
+          }
+        } else {
+          throw new Error('Invalid data format: Expected an array with at least one object');
+        }
       } catch (error) {
         setError(error);
-        console.error('Error fetching patient notes:', error);
       } finally {
         setLoading(false);
       }
