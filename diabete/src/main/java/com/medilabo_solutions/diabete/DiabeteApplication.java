ecb6ecc2751d0071401970f316b6df72f488259c
A-Pogam <alicepogam59@hotmail.com>
2024-09-03 16:16:10 +0200

    Enable Feign in the app

--- diabete/src/main/java/com/medilabo_solutions/diabete/DiabeteApplication.java
+++ diabete/src/main/java/com/medilabo_solutions/diabete/DiabeteApplication.java
@@ -2,8 +2,10 @@ package com.medilabo_solutions.diabete;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DiabeteApplication {
 
 	public static void main(String[] args) {
