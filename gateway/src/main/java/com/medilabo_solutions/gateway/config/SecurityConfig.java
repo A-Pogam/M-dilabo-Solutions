13f1535e8a6d2e9fd314f810ce79b119f0b013da
A-Pogam <alicepogam59@hotmail.com>
2024-09-09 09:30:39 +0200

    tried to make auth works but failed

--- gateway/src/main/java/com/medilabo_solutions/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/medilabo_solutions/gateway/config/SecurityConfig.java
@@ -2,36 +2,58 @@ package com.medilabo_solutions.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http
-                .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/patients/").permitAll()
-                        .anyExchange().authenticated()
-                )
-                .csrf(ServerHttpSecurity.CsrfSpec::disable);
-
-        return http.build();
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.builder()
                 .username("user")
-                .password("password")
-                .roles("USER")
+                .password(bCryptPasswordEncoder().encode("pass"))
+                .authorities("ROLE_USER")
                 .build();
 
-        return new InMemoryUserDetailsManager(user);
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(bCryptPasswordEncoder().encode("pass"))
+                .authorities("ROLE_ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req.requestMatchers("/patients/").permitAll())
+                .authorizeHttpRequests(req -> req.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 }
